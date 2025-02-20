bbb6ce1b983d58cf83088317f865a86353e486bd
Amy Kwon <amygee1209@gmail.com>
2025-02-20 21:02:44 +0900

    [level 2] Title: 주식가격, Time: 26.43 ms, Memory: 19.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def solution(prices):
+    stack = deque([])
+    result = [-1] * len(prices)
+    for i, v in enumerate(prices):
+        while stack and prices[stack[-1]] > v:
+            lastIndex = stack.pop()
+            result[lastIndex] = i - lastIndex
+        stack.append(i)
+    while stack:
+        lastIndex = stack.pop()
+        if result[lastIndex] < 0:
+            result[lastIndex] = len(prices) - 1 - lastIndex
+    return result

1eb08b32c166dd8d237f840c189b7e6d93802146
Amy Kwon <amygee1209@gmail.com>
2025-03-06 20:18:39 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 613.75 ms, Memory: 56.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -0,0 +1,24 @@
+def solution(sequence, k):
+    
+    N = len(sequence)
+    
+    subTotal = [0] * (N+1)
+    for i in range(0, N):
+        subTotal[i+1] = subTotal[i] + sequence[i]
+    
+    left, right = 0, 0
+    result = [left, right]
+    minLen = 9999999
+    while right < len(sequence):
+        intervalSum = subTotal[right+1] - subTotal[left]
+        if intervalSum == k:
+            if (right-left+1) < minLen:
+                minLen = right-left+1
+                result = [left, right]
+            left += 1
+            right += 1
+        elif intervalSum > k:
+            left += 1
+        else:
+            right += 1
+    return result

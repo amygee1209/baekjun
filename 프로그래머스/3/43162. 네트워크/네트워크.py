0551fba669b7f174b4757899cf3c28e44f4ff395
Amy Kwon <amygee1209@gmail.com>
2025-04-15 19:52:09 +0900

    [level 3] Title: 네트워크, Time: 0.67 ms, Memory: 9.36 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(n, computers):
+    N = len(computers)
+    
+    visited = set()
+    cnt = 0
+    
+    for curr in range(N):
+        if curr not in visited:
+            cnt += 1
+            stack = deque([curr])
+
+            while stack:
+                node = stack.popleft()
+                if node not in visited:
+                    visited.add(node)
+
+                    for i, val in enumerate(computers[node]):
+                        if i != node and val == 1:
+                            stack.append(i)
+                            
+    return cnt
+                
